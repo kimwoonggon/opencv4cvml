7aada7620bc9fb27759ced731c680acb85226c7b
김웅곤 <60002940+kimwoonggon@users.noreply.github.com>
2021-05-01 19:21:37 +0900

    Create cartoonfilter.h

---
+++ ch07/cartoonfilter.h
@@ -0,0 +1,9 @@
+#include "opencv2/opencv.hpp"
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+Mat cartoon_filter(const Mat& img);
+Mat pencil_sketch(const Mat& img);
+void cartoon_filter_start();
